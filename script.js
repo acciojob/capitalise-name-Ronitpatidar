a0e97d2929be32a5c8c4e43f51436673089c1c2a
Ronitpatidar <ronitpatidar22@gmail.com>
2025-05-12 10:34:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function convertToUppercase() {
+  // Get the input field by its ID
+  const inputField = document.getElementById("fname");
+
+  // Convert the content to uppercase
+  inputField.value = inputField.value.toUpperCase();
+}
